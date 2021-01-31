5bd6217c710a64792378f71fc909c8d670a3e352
Andi <Andi@GUMPRECHTS-ENGEL>
2021-01-31 18:46:39 +0000

    IcebergTracker versión 2

---
+++ src/icebergtracker/IcebergData.java
@@ -0,0 +1,40 @@
+package icebergtracker;
+
+import java.time.LocalDate;
+
+public class IcebergData {
+    private final LocalDate date;
+    private final Iceberg iceberg;
+    private final String coordinates;
+    private final double volume;
+
+    public IcebergData(LocalDate date, Iceberg iceberg, String coordinates, double volume) {
+        this.date = date;
+        this.iceberg = iceberg;
+        this.coordinates = coordinates;
+        this.volume = volume;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public Iceberg getIceberg() {
+        return iceberg;
+    }
+
+    public String getCoordinates() {
+        return coordinates;
+    }
+
+    public double getVolume() {
+        return volume;
+    }
+
+    @Override
+    public String toString() {
+        return iceberg + ", en fecha: " + date
+                + ", volumen: " + volume + ", cooordenadas: " + coordinates;
+    }
+    
+}
